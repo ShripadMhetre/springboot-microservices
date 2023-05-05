8f055c25d1a0533d5d3778e5e1ea92c76f705432
Shripad_Mhetre <shripadmhetre100@gmail.com>
2023-05-05 23:49:05 +0530

    work in progress

--- gallery-service/src/main/java/com/example/galleryservice/controllers/MainController.java
+++ gallery-service/src/main/java/com/example/galleryservice/controllers/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/gallery")
+@RequestMapping("/")
 public class MainController {
 
     @Autowired
